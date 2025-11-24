748db0f666af4b3e1c4966e830ee27cfbbe569a0
MuhamadFaizNasai <faiznasai828@gmail.com>
2025-11-24 10:03:34 +0700

    latihan pertemuan 11

---
+++ java/variabel.java
@@ -0,0 +1,21 @@
+public class variabel {
+    public static void main(String[] args) {
+                // Variabel dengan tipe data yang sesuai
+        String namaLengkap = "Nama Anda";
+        String tempatTanggalLahir = "Tempat Lahir, Tanggal Lahir";
+        String jenisKelamin = "Jenis Kelamin";
+        String alamatLengkap = "Alamat Lengkap";
+        String noHp = "08123456789";
+        double penghasilanSebulan = 5000000; // contoh angka pendapatan
+        // Menampilkan output sesuai permintaan
+        System.out.println("Assalamu’alaikum");
+        System.out.println("Izinkan saya memperkenalkan diri");
+        System.out.println("Nama Lengkap      : " + namaLengkap);
+        System.out.println("Tempat & Tanggal Lahir : " + tempatTanggalLahir);
+        System.out.println("Jenis Kelamin     : " + jenisKelamin);
+        System.out.println("Alamat Lengkap    : " + alamatLengkap);
+        System.out.println("No Hp             : " + noHp);
+        System.out.println("Penghasilan Sebulan : " + penghasilanSebulan);
+    }
+    
+}
